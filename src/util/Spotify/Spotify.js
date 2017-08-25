4fb240f25417073224d76fff34243c028ca2c844
Kris Erdahl <Erdahl@Kriss-MBP.domain>
2017-08-25 12:18:54 -0500

    update accessToken section again

--- src/util/Spotify/Spotify.js
+++ src/util/Spotify/Spotify.js
@@ -7,34 +7,23 @@ const Spotify = {
 		if (accessToken) {
 			return accessToken;
 		} else {
-			let url = window.location.href;
-			fetch(
-				`https://accounts.spotify.com/authorize?client_id=${clientID}&redirect_uri=${redirectURI}&response_type=token&state=234`
-			).then(resolve => resolve(window.location.href));
-			if (url.indexOf('#access_token') !== -1) {
-				url.match('/access_token=([^&]*)/');
-				accessToken = /access_token=([^&]*)/;
-				console.log(accessToken);
-				url.match('/expires_in=([^&]*)/');
-				let expiresIn = /expires_in=([^&]*)/;
-				console.log(expiresIn);
-				window.setTimeout(() => (accessToken = ''), expiresIn * 1000);
-				window.history.pushState('Access Token', null, '/');
-				window.location = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
-				return window.location;
-			}
+			window.location = `https://accounts.spotify.com/authorize?client_id=${clientID}&redirect_uri=${redirectURI}&response_type=token&state=234`;
+			let accessToken = window.location.href.match(/access_token=([^&]*)/);
+			console.log(accessToken);
+			let expiresIn = window.location.href.match(/expires_in=([^&]*)/);
+			console.log(expiresIn);
+			window.setTimeout(() => (accessToken = ''), expiresIn * 1000);
+			window.history.pushState('Access Token', null, '/');
+			window.location = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
+			return window.location;
 		}
 	},
 	search(term) {
-		Spotify.getAccessToken();
-		return new Promise(resolve => resolve(accessToken))
-			.then(() => {
-				return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
-					headers: {
-						Authorization: `Bearer ${accessToken}`
-					}
-				});
-			})
+		return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+			headers: {
+				Authorization: `Bearer ${accessToken}`
+			}
+		})
 			.then(response => response.json())
 			.then(jsonResponse => {
 				if (jsonResponse.tracks) {
